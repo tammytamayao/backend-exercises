fb15066246681099e8011ae520772b7e777191f9
tammytamayao <tammytamayao@gmail.com>
2022-09-20 10:33:12 +0800

    commit act1

--- activity1/act_1.rb
+++ activity1/act_1.rb
@@ -1,11 +1,11 @@
 #1
-puts "\n\nThis is my solution to item #1"
+puts "\nThis is my solution to item #1"
 
 array = [1,2,3,4,5,6,7,8,9,10]
 array.each { |element| puts element }
 
 #2
-puts "\n\nThis is my solution to item #2"
+puts "\nThis is my solution to item #2"
 
 h = {a:1, b:2, c:3, d:4}
 puts "value of b is #{h[:b]}"
@@ -14,11 +14,10 @@ h[:e]=5
 puts "adding key-value pair {e:5} to hash: #{h}"
 
 #3
-puts "\n\nThis is my solution to item #3"
+puts "\nThis is my solution to item #3"
 
 contact_data = [["john@email.com", "123 Main st.", "555-123-4567"],
             ["avion@email.com", "404 Not Found Dr.", "123-234-3454"]]
-
 contacts = {"John Cruz" => {}, "Avion School" => {}}
 
 i=0
@@ -30,7 +29,7 @@ while i<=1 do
     i+=1
 end
 
-puts " #{contacts}\n\n"
+puts " #{contacts}\n"
 
 #3
-puts "\n\n Check age.rb for my solution to item #4"
+puts "\n Check age.rb for my solution to item #4"
--- activity1/age.rb
+++ activity1/age.rb
@@ -1,8 +1,8 @@
 #4
-puts "\n\nThis is my solution to item #4"
+puts "\nThis is my solution to item #4"
 puts "How old are you?"
 age = gets.to_i
-puts "\n\nInput: #{age}"
+puts "\nInput: #{age}"
 i=1
 while i <= 4 do
     puts "In #{10*i} years you will be: #{(10*i)+age}"
