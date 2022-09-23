feaefa03c7e98ed9c20f905a8ad29968d75412f2
tammytamayao <tammytamayao@gmail.com>
2022-09-23 10:50:17 +0800

    updated-act_3

--- activity3/act_3.rb
+++ activity3/act_3.rb
@@ -7,18 +7,24 @@ class Enroll
         @degree = degree
         @units = units
         @is_paid = false
+        @is_enrolled = false
     end
 
     def enroll_units
+        @is_enrolled = true
         @is_paid = "pending..."
         puts "Total Tuition Fee: PHP #{@units * 2000}"
         puts "Proceed to tuition payment..."
     end
 
     def pay_tuition
-        @is_paid = true
-        puts "Fully Paid!"
-        puts "You are already enrolled for this semester"
+        if @is_enrolled == true
+            @is_paid = true
+            puts "Fully Paid!"
+            puts "You are already enrolled for this semester"
+        else
+            puts "Enroll units first"
+        end
     end
 
 end
