dd8f643aea3868f99a365e41631b3e388de1a2c5
tammytamayao <tammytamayao@gmail.com>
2022-09-25 11:14:39 +0800

    commit act_4

---
+++ activity4/act_4.rb
@@ -0,0 +1,24 @@
+class Confection
+    def prepare
+        puts "Baking at 350 degrees for 25  minutes."
+    end
+    def finish_baking
+        puts "Finish Baking"
+    end
+end
+
+class Cupcake < Confection
+    def apply_frosting
+        puts "Applying frosting"
+    end
+end
+
+class Banana_Cake < Confection
+end
+
+Cupcake_order = Cupcake.new()
+puts "#{Cupcake_order.prepare}\n#{Cupcake_order.apply_frosting}\n#{Cupcake_order.finish_baking}"
+
+Banana_Cake_order = Banana_Cake.new()
+puts "#{Banana_Cake_order.prepare}\n#{Banana_Cake_order.finish_baking}"
+
