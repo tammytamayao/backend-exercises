0310a1a0e0c684d25ac5fc5996466ede702398c9
tammytamayao <tammytamayao@gmail.com>
2022-10-04 19:50:47 +0800

    modified disemvowel_trolls.rb

--- disemvowel_trolls.rb
+++ disemvowel_trolls.rb
@@ -1,4 +1,5 @@
 def disemvowel_trolls (input)
+    
 string_arr = []
     input.chars.each do |letter|
         unless (letter == 'a' || letter == 'A' || letter == 'e' || letter == 'E' || letter == 'i' || letter == 'I' || letter == 'o' || letter == 'O' || letter == 'u' || letter == 'U')
